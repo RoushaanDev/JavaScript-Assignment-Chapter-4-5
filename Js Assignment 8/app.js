f3595624f55303441e935007d5d3ae695e96991b
RoushaanOP <roushaanop@gmail.com>
2025-11-15 00:59:14 +0500

    Add files via upload

---
+++ Js Assignment 8/app.js
@@ -0,0 +1,16 @@
+
+document.write("<h2>4. Simulate a Dice Roll (1-6)</h2>");
+
+var date = new Date();
+var m = date.getTime();
+
+// d: dice value (1-6)
+var d = (m % 10) % 6 + 1;
+
+document.write("random dice value: " + d + "<br>");
+document.write("<hr>");
+
+
+
+Sir Aur Sawal Samaj Nahi Aye Aur Pc Kharab The Aj HI Aya ha 11/15/2025
+isiliye nahi kiye :) Wo Sir Help Le Ke Kronga Baqi Sawalat
